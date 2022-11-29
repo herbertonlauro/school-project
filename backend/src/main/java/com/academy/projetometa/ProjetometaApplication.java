7a3a6fca30454f530db8d142378e67ffb5b8c462
Herberton <herberton.dev@gmail.com>
2022-11-29 19:39:08 -0400

    projeto criado

---
+++ backend/src/main/java/com/academy/projetometa/ProjetometaApplication.java
@@ -0,0 +1,13 @@
+package com.academy.projetometa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetometaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetometaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/academy/projetometa/ProjetometaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.projetometa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetometaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
