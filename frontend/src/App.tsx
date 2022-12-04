78210ac335a83cff10da4359d101e458dbf21ee1
Herberton <herberton.dev@gmail.com>
2022-12-04 17:42:01 -0400

    add react-toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./components/Header";
 import SalesCardAcad from "./components/SalesCradAcad";
 import "./index.css";
@@ -5,6 +7,7 @@ import "./index.css";
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
       <section id="sales">
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './styles.css';
@@ -11,7 +12,7 @@ type Props = {
 function clickEnviar(id :number){
   axios(`${BASE_URL}/vendas/${id}/notificacao`)
   .then(Response => {
-    console.log("sucesso");
+   toast.info("sms enviado ");
   })
 }
 
